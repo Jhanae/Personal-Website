fc2bf3da8d25e3a0c4ea2477114dc8829ed1dcdd
Jhanae <jhanaechisholm12@gmail.com>
2021-05-10 02:08:46 -0400

    Basic Website

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript"; 
 document.querySelector('body').appendChild(h2); 
+
+const header1 = document.createElement('h2');
+header1.id = "description";
+header1.innerHTML = "<h2>Where champiogns shop...<h2>";
